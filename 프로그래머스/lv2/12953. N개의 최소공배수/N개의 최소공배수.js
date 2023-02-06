4ddd967c8971fffb8e64c6995b2e1497bb760e1c
B22MER <74225835+b22mer@users.noreply.github.com>
2023-02-06 22:20:56 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,13 @@
+function getGcd(a, b) {
+  if (b === 0) return a;
+  return getGcd(b, a % b);
+}
+
+function solution(arr) {
+ return arr.reduce((a, b) => (a * b) / getGcd(a, b));
+
+    
+    
+}
+
+// 1 3 4 7 2
