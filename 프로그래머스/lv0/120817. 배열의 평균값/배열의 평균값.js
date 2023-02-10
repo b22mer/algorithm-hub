1fcf29929361d6ddd1968252f18ba183549bf65a
B22MER <74225835+b22mer@users.noreply.github.com>
2023-02-10 23:54:29 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let result=0;
+    for(let i=0; i<numbers.length; i++){
+        result+=numbers[i];
+    }
+    
+    
+    return result/numbers.length;
+    console.log(result)
+    
+}
